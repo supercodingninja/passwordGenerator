2c86eb6677627e2d80d4a73f7e20b38c9093aa41
Frederick Thomas <frederickdthomasjr@gmail.com>
2020-10-24 07:18:40 -0700

    Initial Javascript file added.
    Code is heavily being rendered.  This file uploaded is a placeholder for git commits to be push to.

---
+++ pw.js
@@ -0,0 +1,4 @@
+// Ref. link: [https://stackoverflow.com/questions/24597634/how-to-generate-an-array-of-alphabet-in-jquery]
+var pwBtn
+
+var alphaNumChar = "abcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*?".split("");
