f296e42bb9375663fbb4fa135f08dd595abff9d0
SCN <supercodingninja@outlook.com>
2020-10-25 00:23:23 -0700

    CSS styles added (having trouble with styling size and position).  Site looked better without styles added as far as size; but I believe it has something to do with the JS file, since I commented ALL CSS styles; therefore; I commented out my jQuery; and recieve the results desired!

--- pw.js
+++ pw.js
@@ -46,9 +46,9 @@ pwBtn.addEventListener("click", writePassword);
 alert("click", writePassword);
 
 // jQuery stab in the dark to check event listener.  Ref. Link: https://stackoverflow.com/questions/446892/how-to-find-event-listeners-on-a-dom-node-when-debugging-or-from-the-javascript //
-events = $._data(this, 'click');
- for (writePassword) {
-   events[click].forEach(function (event) {
-     console.log(event['handler']);
-   });
- }
+// events = $._data(this, 'click');
+//  for ("click", writePassword) {
+//    events[click].forEach(function (event) {
+//      console.log(event['handler']);
+//    });
+//  }
